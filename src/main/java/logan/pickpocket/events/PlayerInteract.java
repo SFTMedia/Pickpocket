4f78489b121ed22d8ceb8e72e7a098c91b345fb8
blalp <26471229+blalp@users.noreply.github.com>
2020-06-26 11:30:50 -0700

    respecting worldguard

--- src/main/java/logan/pickpocket/events/PlayerInteract.java
+++ src/main/java/logan/pickpocket/events/PlayerInteract.java
@@ -3,6 +3,12 @@ package logan.pickpocket.events;
 import logan.pickpocket.main.Pickpocket;
 import logan.pickpocket.main.Profile;
 import logan.pickpocket.main.Profiles;
+
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.FlagContext.FlagContextBuilder;
+import com.sk89q.worldguard.protection.flags.StateFlag.State;
+
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -28,6 +34,20 @@ public class PlayerInteract implements Listener {
         if (!player.isSneaking()) return;
         Profile profile = Profiles.get(player, pickpocket.getProfiles());
 
+        if(WorldGuardPlugin.inst().getRegionContainer().createQuery().queryValue(event.getRightClicked().getLocation(), player, new StateFlag("pvp", false)).equals(State.DENY)){
+            System.out.println("blocked from pvp");
+            player.sendMessage("Saftey Samurai: NOPE");
+            event.getRightClicked().sendMessage("Saftey Samurai: "+ player.getName()+" tried to pickpocket you, but I kept you safe while you are in my domain.");
+            return;
+        } else {
+            System.out.println("allowed from pvp");
+        }
+
+        if(event.getRightClicked().hasPermission("pickpocket.staff")) {
+            player.sendMessage("They see you.");
+            event.getRightClicked().sendMessage(player.getName()+" tried to pickpocket you, but you in your ninja ways saw them.");
+            return;
+        }
 
         if (!pickpocket.getCooldowns().containsKey(player) && !profile.canCooldownBypass()) {
             Player entity = (Player) event.getRightClicked();
